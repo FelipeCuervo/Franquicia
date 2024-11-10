47517f44f872c184b090010f5010d8805dcec0c6
felipe <158594659+FelipeCuervo609@users.noreply.github.com>
2024-11-10 01:41:11 -0500

    eliminar import

--- stockFranquicia/src/main/java/com/franquicia/stockfranquicia/controller/ProductoController.java
+++ stockFranquicia/src/main/java/com/franquicia/stockfranquicia/controller/ProductoController.java
@@ -2,7 +2,6 @@ package com.franquicia.stockfranquicia.controller;
 
 import com.franquicia.stockfranquicia.entities.Producto;
 import com.franquicia.stockfranquicia.services.ProductoService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
